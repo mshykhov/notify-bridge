c974f2951dbfa4f2e5395b91a9df9e70536f9686
mshykhov <myronshykhov@gmail.com>
2025-12-12 20:20:42 +0800

    refactor: sync user data on all update types with from

--- src/main/kotlin/com/smhomelab/notifier/handler/UserDataSyncHandler.kt
+++ src/main/kotlin/com/smhomelab/notifier/handler/UserDataSyncHandler.kt
@@ -3,17 +3,23 @@ package com.smhomelab.notifier.handler
 import com.smhomelab.notifier.service.BotUserService
 import io.github.dehuckakpyt.telegrambot.annotation.HandlerComponent
 import io.github.dehuckakpyt.telegrambot.handler.BotUpdateHandler
+import io.github.dehuckakpyt.telegrambot.model.telegram.User
 
 @HandlerComponent
 class UserDataSyncHandler(
     private val botUserService: BotUserService
 ) : BotUpdateHandler({
 
-    message {
+    fun sync(from: User) {
         botUserService.syncUserData(from.id, from.username, from.firstName, from.lastName)
     }
 
-    callbackQuery {
-        botUserService.syncUserData(from.id, from.username, from.firstName, from.lastName)
-    }
+    message { sync(from) }
+    editedMessage { sync(from) }
+    callbackQuery { sync(from) }
+    inlineQuery { sync(from) }
+    chosenInlineResult { sync(from) }
+    myChatMember { sync(from) }
+    chatMember { sync(from) }
+    chatJoinRequest { sync(from) }
 })
