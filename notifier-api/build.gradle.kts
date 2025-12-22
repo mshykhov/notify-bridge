c269406974061191dbb287665ef7e7d044681cbb
mshykhov <myronshykhov@gmail.com>
2025-12-22 23:08:26 +0800

    chore: update rest-client to 0.1.7

--- notifier-api/build.gradle.kts
+++ notifier-api/build.gradle.kts
@@ -63,7 +63,7 @@ dependencyManagement {
 
 dependencies {
     // REST Client
-    api("com.smhomelab:rest-client:0.1.6")
+    api("com.smhomelab:rest-client:0.1.7")
 
     // Spring Boot
     implementation("org.springframework.boot:spring-boot-autoconfigure")
