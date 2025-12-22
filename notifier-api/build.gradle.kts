c807297bc6268c2bfc9ee8b7c0f30ec807d6ae94
mshykhov <myronshykhov@gmail.com>
2025-12-22 20:55:35 +0800

    chore(notifier-api): bump rest-client to 0.1.5

--- notifier-api/build.gradle.kts
+++ notifier-api/build.gradle.kts
@@ -49,7 +49,7 @@ dependencyManagement {
 
 dependencies {
     // REST Client
-    api("com.smhomelab:rest-client:0.1.4")
+    api("com.smhomelab:rest-client:0.1.5")
 
     // Spring Boot
     implementation("org.springframework.boot:spring-boot-autoconfigure")
